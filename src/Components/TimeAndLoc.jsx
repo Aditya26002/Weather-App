9000c849258ea172097ca2cca8078dbf8c4d8c4e
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-06-19 00:29:19 +0530

    Add TimeAndLoc component for displaying time and location

---
+++ src/Components/TimeAndLoc.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TimeAndLoc = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-2xl font-extralight">
+            Tuesday, 19 June 2024 | Local Time: 12:00 PM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-3xl font-medium">Berlin, DE</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimeAndLoc;

6073ec92752ce8fb265f67b5905edcccbdb0fc2f
Aditya Daksh <aditya2012058@akgec.ac.in>
2024-06-19 00:28:55 +0530

    feat: Update App component to include Inputs, TimeAndLoc, and TopButtons components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import Inputs from './Components/Inputs'
+import TimeAndLoc from './Components/TimeAndLoc'
+import TopButtons from './Components/TopButtons'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      
-    </>
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br from-cyan-400 to-blue-700 shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+      <TimeAndLoc />
+    </div>
   )
 }
 
